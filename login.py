7637eb1c967180f03778449c4521d8e689cc6aec
wenrise <1450815411@qq.com>
2022-06-28 13:09:41 +0800

    这是一个点击百度新闻case

---
+++ login.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com')
+time.sleep(5)
+ele = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[3]/a[1]")
+ActionChains(driver).move_to_element(ele).perform()
+ele.click()
